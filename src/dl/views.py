2b2e01157d8df6eb3c55bc2f5b32d3df30612935
PussyMan <alendorf.alex@yandex.ru>
2018-12-14 00:24:58 +0400

    Added template inheritance

--- src/dl/views.py
+++ src/dl/views.py
@@ -1,12 +1,16 @@
 from flask import render_template
+from flask import flash
+from flask import redirect
 from dl import app
 from dl.forms import RegistrationForm
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Data entered is normal, but registration has not yet been added.Sry!')
     return render_template('login.html', form=form)
 
 
