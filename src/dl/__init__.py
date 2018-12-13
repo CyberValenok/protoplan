3e6385246bf399a2e404f458815d2e5171efe828
PussyMan <alendorf.alex@yandex.ru>
2018-12-13 16:53:38 +0400

    It worked

--- src/dl/__init__.py
+++ src/dl/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-from config import Configuration
+#from config import Configuration
 
 
 app = Flask(__name__)
-app.config.from_object(Configuration)
-from module import models
-from app import views
+#app.config.from_object(Configuration)
+#from module import models
+from dl import views
--- src/main.py
+++ src/main.py
@@ -1,1 +1,1 @@
-from app import app
+from dl import app
