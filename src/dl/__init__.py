31221bcf6f37532571dd1d2f9525bdcf5fff0dec
PussyMan <alendorf.alex@yandex.ru>
2018-12-14 01:35:38 +0400

    template models

--- src/dl/__init__.py
+++ src/dl/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(dl)
+migrate = Migrate(dl,db)
 #from module import models
 from dl import views
---
+++ src/dl/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model):
+    id= db.Columb(db.Integer, primary_key=True)
+    username = db.column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash= db.Column(db.String(128))
+
+    
